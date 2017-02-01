98c17b7d1a781e375c4c9c5b7106aa846b62934d
fescalona4 <fescalona4@gmail.com>
2017-01-31 22:12:17 -0500

    adding first form example for reference

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroFormComponent } from './hero-form.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, MaterialModule.forRoot(), FormsModule ],
+  declarations: [ AppComponent, HeroFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
